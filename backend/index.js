8a1311ca8bb3f7fef556ed6cf8a2c3dea064109e
RudraGurung <rudragurung911@gmail.com>
2026-01-30 18:19:57 +0530

    Connection between MongoDB and NodeJS create

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>API is running successfully</h1>");
@@ -22,5 +25,5 @@ app.get("/api/chat/:id", (req, res) => {
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`.yellow.bold);
 });
