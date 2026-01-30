cf63a2e0779f4781f3aadafb9e8f3d80329b31eb
RudraGurung <rudragurung911@gmail.com>
2026-01-30 12:51:56 +0530

    Create Node Server and Express API

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("<h1>API is running successfully</h1>");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
